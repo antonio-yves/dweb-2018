6e5d34faf9b6f5b6e30ff81f8808bbe398763663
Gustavo Soares <gustavo@papaga.io>
2018-07-17 20:19:35 -0300

    session

--- flask/movies/app/views.py
+++ flask/movies/app/views.py
@@ -1,14 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from flask import request, redirect, url_for, render_template
+import psycopg2, psycopg2.extras
+
+from flask import g, session, request, redirect, url_for, render_template
 
 from app import app
 
 lista_filmes = ['Star Wars', 'Toy Story', 'Taxi Driver']
 
-@app.route('/')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Connect database
+# g = http://flask.pocoo.org/docs/1.0/api/#flask.g
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.before_request
+def before_request():
+   g.db = psycopg2.connect("dbname=movies user=guga password=gugasv host=127.0.0.1")
+
+# Disconnect database
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT * FROM genero;")
+    data = cur.fetchall()
+    cur.close()
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if request.method == 'POST':
+        session['name'] = request.form['nome']
+
+    if 'name' in session:
+        name = session['name']
+    else:
+        name = None
+
+    return render_template('index.html', generos=data, username=name)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
