58fe6706896dde66106a590528939b426bfe5a8a
Gustavo Soares <gustavo@papaga.io>
2018-08-17 16:48:08 -0300

    bootstrao

--- django/iflibrary/iflibrary/settings.py
+++ django/iflibrary/iflibrary/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
+
     'app.library',
 ]
 
@@ -120,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
